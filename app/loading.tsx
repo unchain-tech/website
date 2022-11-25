29b89b86db13fbe39cc3bd194bd349d957e740b2
SHØ <neila@users.noreply.github.com>
2022-11-26 01:49:45 +0900

    stable setup

--- app/loading.tsx
+++
@@ -1,6 +0,0 @@
-export default function Loading() {
-  const LoadingSkeleton = () => {
-    return <div />;
-  };
-  return <LoadingSkeleton />;
-}
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="">
+      <div>
+        <Head>
+          <title>UNCHAIN</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div />
+      </div>
+    </div>
+  );
+}

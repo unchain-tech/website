d4290450d2e26c3e71aff38f58c9e645b0867140
SHØ <neila@users.noreply.github.com>
2022-11-27 14:26:18 +0900

    layout

---
+++ components/layouts/baseLayout.tsx
@@ -0,0 +1,54 @@
+import Footer from 'components/molecules/footer';
+import Header from 'components/molecules/header';
+import Head from 'next/head';
+import { FC, ReactNode } from 'react';
+
+type Props = {
+  pageTitle: string;
+  children: ReactNode;
+};
+
+export const Layout: FC<Props> = ({ pageTitle, children }) => {
+  const siteTitle = 'UNCHAIN';
+  const subTitle = 'gm, world';
+
+  return (
+    <>
+      <Head>
+        <title>
+          {pageTitle
+            ? `${pageTitle} | ${siteTitle}`
+            : `${siteTitle} | ${subTitle}`}
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="shiftbase.xyz" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="shiftbase, inc." />
+        <meta
+          property="og:image"
+          content="https://www.unchain.tech/UNCHAIN_banner.png"
+        />
+        <meta
+          property="og:description"
+          content="Learn how to develop and contribute to the decentralized web, earning tokens based on your on-chain activity."
+        />
+        <meta
+          name="description"
+          content="UNCHAIN is a community of passionate builders in the blockchain space."
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@zsh0x" />
+      </Head>
+
+      <Header />
+
+      <main className="my-4">
+        <div className="page-main">{children}</div>
+      </main>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;

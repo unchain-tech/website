e2a5172ba393dbfc21e98bd60396ae1e28ede28f
SHØ <neila@users.noreply.github.com>
2022-11-27 14:44:24 +0900

    add config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   i18n: { locales: ['en', 'ja'], defaultLocale: 'en', localeDetection: false },
+  images: {
+    domains: ['raw.githubusercontent.com'],
+  },
 };
 
 module.exports = nextConfig;

830c2d7e81d138f1772163ad9eed7c5ba2565f7c
SHØ <neila@users.noreply.github.com>
2022-11-27 14:44:11 +0900

    build relative path

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,7 +5,10 @@ const buildLintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
 
-const buildPrettierCommand = (filenames) => `prettier ${filenames.join(' ')}`;
+const buildPrettierCommand = (filenames) =>
+  `prettier ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
 
 module.exports = {
   '**/*.{js,jsx,ts,tsx}': [buildLintCommand],

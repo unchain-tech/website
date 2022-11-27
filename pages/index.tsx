63d075dec0345fe414f853f7d10f4bfd8a2f9347
SHØ <neila@users.noreply.github.com>
2022-11-27 14:42:56 +0900

    clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,45 @@
-import Layout from 'components/layouts/baseLayout';
 import Head from 'next/head';
+import Image from 'next/image';
 
-export default function Home() {
+import { FC } from 'react';
+
+import Layout from 'components/layouts/baseLayout';
+import Collaborators from 'components/sections/collaborators';
+import Landing from 'components/sections/firstview';
+import Joinus from 'components/sections/joinus';
+import Projects from 'components/sections/projects';
+import Services from 'components/sections/services';
+import Values from 'components/sections/values';
+import thumbnailNFT from 'public/thumbnail_nft.png';
+
+export const Home: FC = () => {
   return (
-    <div className="">
-      <div>
-        <Head>
-          <title>UNCHAIN</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Layout pageTitle="Home">
-          <h1 className="text-brand-yellow">UNCHAIN</h1>
-          <h1>YOUR</h1>
-          <h1 className="">POTENTIAL.</h1>
-        </Layout>
-      </div>
+    <div id="page" className="">
+      <Head>
+        <title>UNCHAIN</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Layout pageTitle="Home">
+        <Image
+          src={thumbnailNFT}
+          alt="A thumbnail image of UNCHAIN NFTs"
+          className="absolute -z-10 right-0"
+          width={640}
+          height={640}
+        />
+
+        <div id="body" className="pt-96 desktop:pt-96">
+          <Landing />
+          <Services />
+          <Values />
+          <Projects />
+          <Collaborators />
+          <Joinus />
+        </div>
+      </Layout>
     </div>
   );
-}
+};
+
+export default Home;

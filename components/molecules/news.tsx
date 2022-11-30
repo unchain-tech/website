7b1b3e752e69e6caf3748da4502cec1c44791a55
SHØ <neila@users.noreply.github.com>
2022-12-01 02:22:28 +0900

    news

---
+++ components/molecules/news.tsx
@@ -0,0 +1,63 @@
+import Link from 'next/link';
+
+import { FC } from 'react';
+
+export const News = () => {
+  const NewsItem: FC<{ url: string; description: string }> = ({
+    url,
+    description,
+  }) => {
+    return (
+      <Link
+        href={url}
+        passHref={true}
+        className="py-4 hover:no-underline hover:text-brand-sky"
+        target="_blank"
+      >
+        <div className="hover:border border-brand-sky rounded-lg p-1">
+          <div className="flex flex-row py-2 justify-between">
+            <p className="font-sans truncate pr-8">{description}</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-8 stroke-brand-yellow"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+              />
+            </svg>
+          </div>
+          <hr className="h-px border-0 bg-brand-pink" />
+        </div>
+      </Link>
+    );
+  };
+
+  const newslinks = [
+    'https://unchain-shiftbase.notion.site/UNCHAIN-Build2Earn-on-Astar-Network-5b25608cccd549dea4609ee9cb87b52a',
+    'https://unchain-shiftbase.notion.site/UNCHAIN-Bug-Bounty-Bootcamp-BBB-Cohort1-52247e1e2f904f89a5022dd24aef9c73',
+  ];
+  const newsdescriptions = [
+    //"dApps開発を学ぶと、$ASTRがもらえる！UNCHAIN developer's grant 開始のお知らせ",
+    `#Build2Earn on Astar Network - dAppを開発して1000$ASTRを獲得しよう！`,
+    'BBB (Bug Bounty Bootcamp) 開催のお知らせ - スマートコントラクトのセキュリティ技術を学び、バグを見つけて賞金獲得しよう！',
+  ];
+
+  return (
+    <div id="news container" className="py-8">
+      <h2 className="text-brand-yellow py-4">NEWS</h2>
+      {newslinks.map((url, index) => (
+        <NewsItem
+          key={`news ${index + 1}`}
+          url={url}
+          description={newsdescriptions[index]}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default News;
--- components/sections/firstview.tsx
+++ components/sections/firstview.tsx
@@ -2,27 +2,40 @@ import Link from 'next/link';
 
 import { FC } from 'react';
 
+import News from 'components/molecules/news';
+
 export const Landing: FC = () => {
   return (
     <div id="firstview">
-      <div id="container" className="px-6 pb-8 desktop:px-32">
-        <div id="slogan" className="pt-4 pb-2 desktop:pt-0 desktop:pb-8">
-          <h1 className="text-brand-yellow">UNCHAIN</h1>
-          <h1>your</h1>
-          <h1 className="">potential.</h1>
+      <div
+        id="section container"
+        className="pt-96 desktop:pt-96 pb-8 px-6 desktop:px-32"
+      >
+        <div id="first view" className="pb-8">
+          <div id="slogan" className="pt-8 pb-4">
+            <h1 className="text-brand-yellow">UNCHAIN</h1>
+            <h1>your</h1>
+            <h1 className="">potential.</h1>
+          </div>
+
+          <p
+            id="blurb"
+            className="text-xs desktop:text-lg py-2 desktop:py-4 w-5/6 desktop:w-1/2"
+          >
+            UNCHAINは、NFTの作成やdAppsの構築など、プロジェクト開発を通してweb3を学び、実践経験を積みながら自分のアイデアを形にする力を身につけるエンジニアのためのコミュニティです。
+          </p>
+
+          <Link
+            href="https://airtable.com/shrKEXFRup9ZqF3GY"
+            passHref={true}
+            target="_blank"
+          >
+            <button className="btn bg-brand-red hover:bg-brand-blue my-2 desktop:my-4">
+              Join the community
+            </button>
+          </Link>
         </div>
-        <p id="blurb" className="w-5/6 py-2 desktop:py-4 desktop:w-2/5">
-          UNCHAINは、NFTの作成やdAppsの構築など、プロジェクト開発を通してweb3を学び、実践経験を積みながら自分のアイデアを形にする力を身につけるエンジニアのためのコミュニティです。
-        </p>
-        <Link
-          href="https://airtable.com/shrKEXFRup9ZqF3GY"
-          passHref={true}
-          target="_blank"
-        >
-          <button className="btn bg-brand-red hover:bg-brand-blue my-2 desktop:mb-4">
-            Join the community
-          </button>
-        </Link>
+        <News />
       </div>
     </div>
   );

c48c37fd5fcd2dffc1864809da5061db4cf8ed61
SHØ <neila@users.noreply.github.com>
2022-12-01 00:36:28 +0900

    networks marquee

--- components/molecules/supportednetworks.tsx
+++ components/molecules/supportednetworks.tsx
@@ -8,22 +8,49 @@ import { useWindowDimensions } from 'hooks/useWindowDimensions';
 const Chain: FC<{ chain: string; url: string }> = ({ chain, url }) => {
   const { width } = useWindowDimensions();
   return (
-    <Link href={url}>
+    <Link href={url} target="_blank" passHref={true}>
       <Image
         src={`/chain/${chain}.png`}
         alt={`${chain} logo`}
-        width={width! > 481 ? 320 : 140}
-        height={140}
+        width={width! > 481 ? 240 : 120}
+        height={60}
       />
     </Link>
   );
 };
 
 export const SupportedNetworks = () => {
+  const chains = [
+    'astar',
+    'avalanche',
+    'ethereum',
+    'icp',
+    'near',
+    'polygon',
+    'solana',
+  ];
+  const links = [
+    'https://astar.network/',
+    'https://www.avax.network/',
+    'https://ethereum.org/',
+    'https://internetcomputer.org/',
+    'https://near.org/',
+    'https://polygon.technology/',
+    'https://solana.com/',
+  ];
   return (
     <div id="supported networks" className="bg-brand-white">
-      <div id="container" className="flex flex-col py-4">
-        {/* <Chain chain="astar" url="https://astar.network" /> */}
+      <div id="container" className="overflow-x-hidden relative">
+        <div className="flex flex-row items-center px-1 py-2 laptop:p-4 space-x-2 laptop:space-x-8 animate-marquee">
+          {chains.map((chain, index) => (
+            <Chain key={`${chain} logo`} chain={chain} url={links[index]} />
+          ))}
+        </div>
+        <div className="absolute top-0 flex flex-row items-center px-1 py-2 laptop:p-4 space-x-2 laptop:space-x-8 animate-marquee2">
+          {chains.map((chain, index) => (
+            <Chain key={`${chain} logo`} chain={chain} url={links[index]} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -78,7 +78,7 @@ module.exports = {
       },
       keyframes: {
         marquee: {
-          '0%': { transform: 'translateX(100%)' },
+          '0%': { transform: 'translateX(0%)' },
           '100%': { transform: 'translateX(-100%)' },
         },
         marquee2: {

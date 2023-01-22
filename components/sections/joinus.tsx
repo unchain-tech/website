d727ddd80b209bf8ca70abc9dca2961a90dd0388
SHØ <neila@users.noreply.github.com>
2023-01-23 00:22:34 +0900

    move news to last section

--- components/sections/joinus.tsx
+++ components/sections/joinus.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 
 import { FC } from 'react';
 
+import News from 'components/molecules/news';
 import { useWindowDimensions } from 'hooks/useWindowDimensions';
 import UNCHAIN_icon from 'public/UNCHAIN_icon.png';
 
@@ -24,8 +25,12 @@ export const Joinus = () => {
 
   return (
     <div id="Join us" className="relative bg-brand-onyx text-brand-white">
-      <div id="container" className="px-6 py-16 space-y-8">
-        <div id="slogan and logo" className="flex flex-row justify-between">
+      <div id="container" className="px-6 space-y-8 pb-16">
+        <News />
+        <div
+          id="slogan and logo"
+          className="pt-16 flex flex-row justify-between"
+        >
           <div id="slogan" className="py-4 flex flex-col">
             <Slogan text="Join" />
             <Slogan className="ml-32 desktop:ml-64" text="the" />
@@ -34,29 +39,19 @@ export const Joinus = () => {
               <Slogan text="unity." />
             </div>
           </div>
-
-          {width! > 769 ? (
-            <Image
-              src={UNCHAIN_icon}
-              alt="UNCHAIN logo"
-              className="absolute right-0"
-              width={width! / 4}
-              height={width! / 4}
-            />
-          ) : (
-            <Image
-              src={UNCHAIN_icon}
-              alt="UNCHAIN logo"
-              className="absolute top-5 right-0"
-              width={width! / 3}
-              height={width! / 3}
-            />
-          )}
+          <Image
+            src={UNCHAIN_icon}
+            alt="UNCHAIN logo"
+            className="absolute right-6"
+            width={width! / 4}
+            height={width! / 4}
+          />
         </div>
+
         <div className="flex justify-center hover:no-underline">
           <Link href="/api/apply" passHref>
             <button className="btn bg-brand-red hover:bg-brand-blue px-16 py-4 text-2xl">
-              Apply for entry
+              Join the Community
             </button>
           </Link>
         </div>

998c6a8d208a8823d4ca589dd964d563e926d775
raida <raida@student.42.jp>
2023-01-26 15:23:24 +0900

    add GTM tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,13 @@
+import { googleTagManagerId } from '@/lib/gtag';
+import GoogleTagManager from 'components/utils/gtm';
 import { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
-
-import { useEffect } from 'react';
-
-import * as gtag from 'src/lib/gtag';
 import '/public/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouterChange = (url: any) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouterChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouterChange);
-    };
-  }, [router.events]);
 
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-           window.dataLayer = window.dataLayer || [];
-           function gtag(){dataLayer.push(arguments);}
-           gtag('js', new Date());
-           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
-           `,
-        }}
-      />
+      <GoogleTagManager googleTagManagerId={googleTagManagerId}/>
       <Component {...pageProps} />;
     </>
   );
--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,16 +1,8 @@
-interface Window {
-  gtag(
-    _: string,
-    GA_MEASUREMENT_ID: string,
-    pagePath: { page_path: string },
-  ): unknown;
+export const googleTagManagerId =
+  process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || '';
+
+declare global {
+  interface Window {
+    dataLayer: Record<string, unknown>[];
+  }
 }
-declare const window: Window;
-
-export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || '';
-
-export const pageview = (url: string) => {
-  window.gtag('config', GA_MEASUREMENT_ID, {
-    page_path: url,
-  });
-};

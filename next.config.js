66533d883a8e732ada933ec7cd84976aeeb0e312
SHØ <neila@users.noreply.github.com>
2023-02-02 17:34:51 +0900

    add config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   i18n: { locales: ['en', 'ja'], defaultLocale: 'en', localeDetection: false },
   images: {
-    domains: ['raw.githubusercontent.com'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'raw.githubusercontent.com' },
+      { protocol: 'https', hostname: 's3.*.amazonaws.com' },
+      { protocol: 'https', hostname: 'prtimes.jp' },
+    ],
   },
 };
 

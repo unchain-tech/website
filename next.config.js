a80b2c228c04d136bb40990ad6a45563c0b68661
SHØ <neila@users.noreply.github.com>
2023-02-02 17:43:44 +0900

    remove i18n

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: { locales: ['en', 'ja'], defaultLocale: 'en', localeDetection: false },
+  // i18n: { locales: ['en', 'ja'], defaultLocale: 'en', localeDetection: false },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'raw.githubusercontent.com' },

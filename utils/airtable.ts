b9ebc8922359deffd73f67c3d928ad69a287e5fd
SHØ <neila@users.noreply.github.com>
2023-04-07 01:44:00 +0900

    refactor

--- utils/airtable.ts
+++ utils/airtable.ts
@@ -6,6 +6,7 @@ import Airtable from 'airtable';
 const base_unchainApplication = new Airtable({
   apiKey: process.env.AIRTABLE_API_KEY,
 }).base(process.env.AIRTABLE_BASE_UNCHAINAPPLICATION as string);
+
 const table_unchainApplication = base_unchainApplication(
   process.env.AIRTABLE_TABLE_UNCHAINAPPLICATION as string,
 );

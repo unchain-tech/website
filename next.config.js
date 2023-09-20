bc73d1fd924488cc826404f58a371845819ddc07
neila <neila@shiftbase.xyz>
2023-09-21 01:08:49 +0900

    add news

--- next.config.js
+++ next.config.js
@@ -6,9 +6,10 @@ const nextConfig = {
   // i18n: { locales: ['en', 'ja'], defaultLocale: 'en', localeDetection: false },
   images: {
     remotePatterns: [
+      { protocol: 'https', hostname: 'assets.st-note.com' },
+      { protocol: 'https', hostname: 'prtimes.jp' },
       { protocol: 'https', hostname: 'raw.githubusercontent.com' },
       { protocol: 'https', hostname: 's3.*.amazonaws.com' },
-      { protocol: 'https', hostname: 'prtimes.jp' },
     ],
   },
 };

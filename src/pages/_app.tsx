6408b97a6e5fc8239d2ef6723611138cc5a1cab7
kiri-42 <t.kirihara0314@gmail.com>
2023-10-25 21:09:03 +0900

    gaの記述を修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,9 @@ import { AppProps } from 'next/app';
 
 import '/public/styles/globals.css';
 
-import GoogleAnalytics from '@/components/GoogleAnalytics';
-
 const UNCHAIN_LP = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ? (
-        <GoogleAnalytics
-          GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}
-        />
-      ) : null}
       <Component {...pageProps} />
     </>
   );

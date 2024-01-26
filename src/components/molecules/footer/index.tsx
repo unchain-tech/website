9b22cd11847d49c99cffc9f017b75a2166dfce40
neila <40727091+neila@users.noreply.github.com>
2024-01-26 15:55:52 +0900

    update footer

--- src/components/molecules/footer/index.tsx
+++ src/components/molecules/footer/index.tsx
@@ -1,33 +1,22 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import shiftbase_logo from 'public/sb_logoname.png';
 import { FC } from 'react';
 import { SocialIcon } from 'react-social-icons';
 
 export const Footer: FC = () => {
   const Socials = () => {
     return (
-      <div className="flex gap-4 p-2">
-        <SocialIcon
-          url="https://twitter.com/UNCHAIN_tech"
-          bgColor="#1DA1F2"
-          fgColor="transparent"
-          label="Twitter"
-          target="_blank"
-          rel="noreferrer"
-        />
+      <div className="space-x-4">
         <SocialIcon
           url="https://discord.gg/w3AyyvKypT"
-          bgColor="#7289DA"
-          fgColor="white"
+          bgColor="white"
+          fgColor="#7289DA"
           label="Discord"
           target="_blank"
           rel="noreferrer"
         />
         <SocialIcon
           url="https://github.com/unchain-tech"
-          bgColor="#000000"
-          fgColor="transparent"
+          bgColor="transparent"
+          fgColor="#000000"
           label="Github"
           target="_blank"
           rel="noreferrer"
@@ -36,63 +25,15 @@ export const Footer: FC = () => {
     );
   };
 
-  const SbProdTag = () => {
-    return (
-      <div id="company" className="flex items-center">
-        <h3 className="font-sans">powered by</h3>
-        <Image src={shiftbase_logo} alt="shiftbase logo" width={160} />
-      </div>
-    );
-  };
-
-  const urls = [
-    'https://unchain-shiftbase.notion.site/UNCHAIN-20230126-aed99b6ac88642abae05b98d821edda8',
-    'https://unchain-shiftbase.notion.site/UNCHAIN-20230126-fa9d10f2a932424eb33a46f927f92d9f',
-    '/about/sct',
-    'https://shiftbase.xyz/careers',
-    'https://shiftbase.xyz/',
-  ];
-  const texts = [
-    'Terms & Conditions',
-    'Privacy Policy',
-    'SCT Disclosure',
-    'Careers',
-    'About shiftbase',
-  ];
-
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="py-4">
-      <div
-        id="shiftbase"
-        className="space-y-4 bg-brand-white px-4 py-4 text-brand-onyx"
-      >
-        <div className="flex flex-col-reverse items-center laptop:flex-row laptop:justify-between">
-          <SbProdTag />
-          <Socials />
+    <footer className="py-4 bg-white">
+      <div className="text-center space-y-2">
+        <Socials />
+        <div className="font-sans text-black text-sm desktop:text-base">
+          {currentYear} &copy; UNCHAIN - Licensed under MIT license.
         </div>
-
-        <div id="links" className="flex flex-col space-y-2">
-          {texts.map((t, i) => {
-            return (
-              <div key={`${t}`}>
-                <Link
-                  href={urls[i]}
-                  passHref={true}
-                  className="cursor-pointer font-sans hover:text-brand-blue text-base font-normal"
-                  target={urls[i].slice(0, 1) === '/' ? '_self' : '_blank'}
-                >
-                  {t}
-                </Link>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-
-      <div className="mx-auto mt-6 text-center font-sans text-sm desktop:text-base">
-        {currentYear} &copy; shiftbase Inc. All rights reserved.
       </div>
     </footer>
   );
